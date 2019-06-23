d3cc590cf56bb54af3f2e0db7de1099915c070a5
xiaofeng333 <programmer_feng@foxmail.com>
2019-06-23 21:08:12 +0800

    cors

---
+++ src/main/java/com/feng/springboot/cors/MyConfiguration.java
@@ -0,0 +1,23 @@
+package com.feng.springboot.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyConfiguration {
+
+    /**
+     * 跨域设置
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/hello/**");
+            }
+        };
+    }
+}
